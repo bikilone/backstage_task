d40186a46b6fde7aa29bc78513029106604ab0dd
biki <bikidevtechnologies@gmail.com>
2024-06-09 16:59:39 +0200

    Added margins for header on products page

--- pages/products/index.vue
+++ pages/products/index.vue
@@ -11,9 +11,11 @@ const preloadImage = computed(() => {
 </script>
 
 <template>
-  <div>
-    <h1>Products</h1>
-    <div class="grid gap-4 md:gap-6  grid-cols-1 md:grid-cols-2 lg:grid-cols-3 mx-10 my-20 md:mx-30 xl:mx-40">
+  <div class="mx-10 md:mx-30 xl:mx-40">
+    <h1 class="mt-10 lg:mt-20">
+      Products
+    </h1>
+    <div class="grid gap-4 md:gap-6  grid-cols-1 md:grid-cols-2 lg:grid-cols-3 my-10 lg:my-20">
       <Product
         v-for="product in products"
         :key="product.id"
