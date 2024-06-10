95b2c2e1a40f975d357df1eb05fd4253468f94cc
biki <bikidevtechnologies@gmail.com>
2024-06-10 18:41:49 +0200

    Added basic server side error handling

--- server/api/products.post.ts
+++ server/api/products.post.ts
@@ -2,6 +2,7 @@ import fs from 'fs';
 import path from 'path';
 import { PrismaClient } from '@prisma/client';
 import formidable from 'formidable';
+import { MAX_IMAGE_SIZE } from '~/utills/constants';
 
 const prisma = new PrismaClient();
 
@@ -12,9 +13,27 @@ if (!fs.existsSync(uploadDir)) {
 }
 
 export default defineEventHandler(async (event) => {
-	const form = formidable({ multiples: false, uploadDir, maxFileSize: 1 * 1024 * 1024 });
-
+	const form = formidable({ multiples: false, uploadDir });
 	const [fields, files] = await form.parse(event.req);
+
+	if (!fields.title?.[0]) {
+		throw createError({ statusCode: 400, statusMessage: 'No product title has been sent' })
+	}
+	if (!fields.description?.[0]) {
+		throw createError({ statusCode: 400, statusMessage: 'No product description has been sent' })
+	}
+	if (!fields.price) {
+		throw createError({ statusCode: 400, statusMessage: 'No product price has been sent' })
+	}
+	if (fields.price[0] <= 0) {
+		throw createError({ statusCode: 400, statusMessage: 'The price must be greater than 0' })
+	}
+	if (!files.image?.[0]) {
+		throw createError({ statusCode: 400, statusMessage: 'No product image has been sent' })
+	}
+	if (files.image[0].size > MAX_IMAGE_SIZE) {
+		throw createError({ statusCode: 400, statusMessage: 'The image is too large' })
+	}
 
 	let imageUrl = null;
 	if (files.image.length) {
@@ -28,6 +47,8 @@ export default defineEventHandler(async (event) => {
 		price: parseFloat(fields.price[0]),
 		image: imageUrl,
 	};
+
 	const product = await prisma.product.create({ data });
+
 	return product;
 })
